15d64765256e47641763004a894dad564ace8951
XEON <evandro535@gmail.com>
2024-12-19 18:01:25 -0300

    Adicionar arquivos de projeto.

---
+++ VendasPodutos/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace VendasPodutos
+{
+    internal class Produto
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+        
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+              public double ValorTotalemEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+                + ", $ " 
+                + Preco.ToString("F2", CI) 
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ " 
+                + ValorTotalemEstoque().ToString("F2", CI);
+
+        }
+
+    }
+}
